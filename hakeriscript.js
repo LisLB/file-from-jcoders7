17c15300452c04ccfe8f45574d8fc8fb4607da6a
LisLB <lisbakija@gmail.com>
2025-11-03 09:38:53 -0800

    Add files via upload

---
+++ hakeriscript.js
@@ -0,0 +1,14 @@
+var myPass = "Lis1234567";
+var enterPass = prompt("Enter ur password:");
+
+while (enterPass !== myPass) {
+    enterPass = prompt("Enter ur password:");
+
+    if(enterPass === myPass)
+        alert("welcome to my Page:)")
+}
+
+
+
+// !==
+// while loop
